9de768ad33d838e918f73efa717231560e1ee156
JoF24 <ferdi24joe@gmail.com>
2024-09-27 19:54:59 +0700

    upload script

---
+++ Bagian 1/javascript-array-challenge-main/scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,23 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+  let mobil = cars.slice();
+
+  // Tulis code-mu disini
+  let n = mobil.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (mobil[j].year < mobil[j + 1].year) {
+        let temp = mobil[j];
+        mobil[j] = mobil[j + 1];
+        mobil[j + 1] = temp;
+      }
+    }
+  }
+  // Rubah code ini dengan array hasil sorting secara descending
+  return mobil;
+}
